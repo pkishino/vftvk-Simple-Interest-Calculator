8bc90e6e1210251186bb73c23b418517901720e2
Patrick Kishino <patrick.a.kishino@gmail.com>
2021-11-22 21:00:46 +0900

    Comments added

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
+//Function to compute the interest
 function compute() {
+    //fetch set values
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
+
+    //calulate the interest earned
     var interest = principal * years * rate / 100;
+
+    //convert the set year to actual years
     var year = new Date().getFullYear() + parseInt(years);
 
+    //Output the result and highlight figures
     document.getElementById("result").innerHTML = "If you deposit <mark>" +
      principal + "</mark>,\<br\>at an interest rate of <mark>" + rate + 
      "%\</mark>.<br\>You will receive an amount of <mark>" + interest + 
      "</mark>,\<br\>in the year <mark>" + year + "\</mark><br\>"
 }
+//function to dynamically show the slider value 
 function updateRate() {
-    var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText = rateval;
+    document.getElementById("rate_val").innerText = document.getElementById("rate").value;
 }
 
+//Function checks that principal field is neither empty nor a non-positive number
+//
 function validatePrincipal(){
-    var principal = document.getElementById("principal").value;
-    if (principal <= 0) {
+    var principal = document.getElementById("principal");
+    if (principal == "" || principal.value <= 0) {
         alert("Enter a positive number");
-        document.getElementById("principal").focus();
+        principal.focus();
+        principal.value="";
       }
 }
